8ffdfe28e9547b0b5d508ead02d5f01f1dc8dd6f
Aaron Torres <aarontorres1423@gmail.com>
2023-12-01 12:56:50 -0600

    Add 'Copy Group ID' button to settings screen

--- app/screens/SettingsScreen.jsx
+++ app/screens/SettingsScreen.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import * as Clipboard from "expo-clipboard";
 import { FIREBASE_AUTH } from "../../firebaseConfig";
 
-const SettingsScreen = ({ navigation }) => {
+const SettingsScreen = ({ navigation, route }) => {
+  const groupId = route.params?.groupId;
+
   const handleSignOut = async () => {
     try {
       await FIREBASE_AUTH.signOut();
-      // Navigate to the Login screen after successful sign-out
       navigation.reset({
         index: 0,
         routes: [{ name: "LandingPage" }],
@@ -17,8 +19,19 @@ const SettingsScreen = ({ navigation }) => {
     }
   };
 
+  const handleCopyGroupId = () => {
+    Clipboard.setStringAsync(groupId);
+    Alert.alert("Copied", `Group ID has been copied to clipboard.`);
+  };
+
   return (
     <View style={styles.container}>
+      {groupId && (
+        <TouchableOpacity style={styles.copyButton} onPress={handleCopyGroupId}>
+          <Text style={styles.buttonText}>Copy Group ID</Text>
+        </TouchableOpacity>
+      )}
+
       <TouchableOpacity style={styles.loginButton} onPress={handleSignOut}>
         <Text style={styles.buttonText}>Sign Out</Text>
       </TouchableOpacity>
@@ -38,12 +51,18 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     width: "100%",
   },
+  copyButton: {
+    backgroundColor: "#2a9df4",
+    padding: 15,
+    borderRadius: 5,
+    width: "100%",
+    marginBottom: 10,
+  },
   buttonText: {
     color: "white",
     textAlign: "center",
     fontSize: 18,
   },
-  // ... add other styles as needed
 });
 
 export default SettingsScreen;
