0b30edb9957819de1913be66f0d2bd1c7e4f749e
Aaron Torres <aarontorres1423@gmail.com>
2023-12-05 11:20:23 -0600

    Remove credentials

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX4XSFLdZEqNbRnJLbIf8Woa7hn35bK9Y",
-  authDomain: "homiehub-2f321.firebaseapp.com",
-  projectId: "homiehub-2f321",
-  storageBucket: "homiehub-2f321.appspot.com",
-  messagingSenderId: "310676333279",
-  appId: "1:310676333279:web:a758df8ff6c09d78d7ff6a",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
